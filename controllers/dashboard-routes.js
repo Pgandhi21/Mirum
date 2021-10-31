3ff40df369de4408d9e5bd5cd74e19bcb70c0cdf
Preksha Gandhi <prekshagandhi@gmail.com>
2021-10-31 18:26:06 -0400

    Update custon question query

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -8,8 +8,8 @@ const exampleData = require("../seeds/data");
 router.get("/:id", async (req, res) => {
   console.log(req.params.id);
   try {
-    const data = await exampleData;
-    // const data = await resultsQuery(req.params.id);
+    // const data = await exampleData;
+    const data = await resultsQuery(req.params.id);
     console.log(data);
     res.render("results", {
       layout: "resultsPage",
@@ -89,7 +89,7 @@ async function resultsQuery(subject) {
       // console.log(overflowData);
       // console.log(overflowData.length);
     } catch (error) {
-      console.log('error');
+      console.log("error");
     }
   }
   console.log(overflowData.length);
