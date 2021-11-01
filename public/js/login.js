9326fb99b5b592cf91723dc5e8f2d610b9954532
Preksha Gandhi <prekshagandhi@gmail.com>
2021-10-31 21:46:23 -0400

    User login and signup with sessions and cookies

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+const loginFormHandler = async function (event) {
+  event.preventDefault();
+
+  const usernameEl = document.querySelector("#inputEmail2");
+  const passwordEl = document.querySelector("#inputPassword2");
+
+  const response = await fetch("/login", {
+    method: "POST",
+    body: JSON.stringify({
+      username: usernameEl.value,
+      password: passwordEl.value,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/search");
+  } else {
+    alert("Failed to login");
+  }
+};
+
+document
+  .querySelector("#logIn")
+  .addEventListener("submit", loginFormHandler);
